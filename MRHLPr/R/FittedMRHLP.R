bba5835e197afb0a8c3e4d7faa19c2e0ae448490
Florian Lecocq <florian.lecocq@outlook.com>
2019-04-15 17:00:34 +0200

    Add plots

--- MRHLPr/R/FittedMRHLP.R
+++ MRHLPr/R/FittedMRHLP.R
@@ -7,33 +7,46 @@ FittedMRHLP <- setRefClass(
   ),
   methods = list(
     plot = function() {
+      yaxislim <- c(min(modelMRHLP$Y) - 2 * mean(sqrt(apply(modelMRHLP$Y, 2, var))), min(modelMRHLP$Y) + 2 * mean(sqrt(apply(modelMRHLP$Y, 2, var))))
+
+      # Data, regressors, and segmentation
       par(mfrow = c(2, 1))
-      plot.default(modelMRHLP$Y, type = "l", ylab = "y", xlab = "")
-      title(main = "Time series, MRHLP regimes and process probabilities")
+      matplot(modelMRHLP$Y, type = "l", lty = 1, col = gray.colors(modelMRHLP$m), ylab = "y", xlab = "")
+      title(main = "Time series, MRHLP regimes, and process probabilites")
       colors = rainbow(modelMRHLP$K)
       for (k in 1:modelMRHLP$K) {
-        index <- (StatMRHLP$klas == k)
-        polynomials <- StatMRHLP$polynomials[(StatMRHLP$klas == k), k]
-        lines(StatMRHLP$polynomials[, k], lty = "dotted", lwd = 2, col = colors[k])
-        lines(seq(1:modelMRHLP$m)[index], polynomials, lwd = 2, col = colors[k])
-      }
-
-      plot.default(StatMRHLP$piik[, 1], type = "l", lwd = 2, col = colors[1], xlab = "x", ylab = expression('Probability ' ~ pi [k] (t, w)))
-      if (K > 1) {
-        for (k in 2:modelMRHLP$K) {
-          lines(StatMRHLP$piik[, k], type = "l", lwd = 2, col = colors[k])
+        index <- (statMRHLP$klas == k)
+        for (d in 1:modelMRHLP$m) {
+          polynomials <- statMRHLP$polynomials[(statMRHLP$klas == k), d, k]
+          lines(statMRHLP$polynomials[, d, k], lty = "dotted", lwd = 2, col = colors[k])
+          lines(seq(1:modelMRHLP$n)[index], polynomials, lwd = 2, col = colors[k])
         }
       }
 
-      par(mfrow = c(2, 1))
-      plot.default(modelMRHLP$Y, type = "l", ylab = "y", xlab = "")
-      title(main = "Time series, estimated RHLP model, and segmentation")
+      # Probablities of the hidden process (segmentation)
+      plot.default(statMRHLP$piik[, 1], type = "l", lwd = 2, col = colors[1], xlab = "x", ylab = expression('Probability ' ~ pi [k] (t, w)))
+      if (modelMRHLP$K > 1) {
+        for (k in 2:modelMRHLP$K) {
+          lines(statMRHLP$piik[, k], type = "l", lwd = 2, col = colors[k])
+        }
+      }
 
-      tk = which(diff(StatMRHLP$klas) != 0)
+      # Data, regression model, and segmentation
+      par(mfrow = c(2, 1))
+      matplot(modelMRHLP$Y, type = "l", lty = 1, col = gray.colors(modelMRHLP$m), ylab = "y", xlab = "")
+      title(main = "Time series, estimated MRHLP model, and segmentation")
+
+      # Transition time points
+      tk = which(diff(statMRHLP$klas) != 0)
       for (i in 1:length(tk)) {
         abline(v = tk[i], lty = "dotted", lwd = 2, col = "red")
       }
-      plot.default(StatMRHLP$klas, type = "l", lwd = 2, col = "red", xlab = "", ylab = "Estimated class labels")
+      # Probablities of the hidden process (segmentation)
+      plot.default(statMRHLP$klas, type = "l", lwd = 2, col = "red", xlab = "", ylab = "Estimated class labels")
+
+      # Model log-likelihood during EM
+      par(mfrow = c(1, 1))
+      plot.default(unlist(statMRHLP$stored_loglik)[5:35], type = "l", col = "blue", xlab = "EM iteration number", ylab = "log-lokelihodd")
     }
   )
 )
