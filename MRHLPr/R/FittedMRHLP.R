1ca33c2142a9872ef3336b07e3133facfbc7f8da
Florian Lecocq <florian.lecocq@outlook.com>
2019-04-30 16:47:45 +0200

    Improve plots

--- MRHLPr/R/FittedMRHLP.R
+++ MRHLPr/R/FittedMRHLP.R
@@ -7,46 +7,48 @@ FittedMRHLP <- setRefClass(
   ),
   methods = list(
     plot = function() {
-      yaxislim <- c(min(modelMRHLP$Y) - 2 * mean(sqrt(apply(modelMRHLP$Y, 2, var))), min(modelMRHLP$Y) + 2 * mean(sqrt(apply(modelMRHLP$Y, 2, var))))
+
+      yaxislim <- c(min(modelMRHLP$Y) - 2 * mean(sqrt(apply(modelMRHLP$Y, 2, var))), max(modelMRHLP$Y) + 2 * mean(sqrt(apply(modelMRHLP$Y, 2, var))))
 
       # Data, regressors, and segmentation
-      par(mfrow = c(2, 1))
-      matplot(modelMRHLP$Y, type = "l", lty = 1, col = gray.colors(modelMRHLP$m), ylab = "y", xlab = "")
+      par(mfrow = c(2, 1), mai = c(0.6, 1, 0.5, 0.5), mgp = c(2, 1, 0))
+      matplot(modelMRHLP$Y, type = "l", ylim = yaxislim, xlab = "x", ylab = "y", col = gray.colors(modelMRHLP$m), lty = 1)
       title(main = "Time series, MRHLP regimes, and process probabilites")
-      colors = rainbow(modelMRHLP$K)
+      colorsvec <- rainbow(modelMRHLP$K)
       for (k in 1:modelMRHLP$K) {
         index <- (statMRHLP$klas == k)
         for (d in 1:modelMRHLP$m) {
-          polynomials <- statMRHLP$polynomials[(statMRHLP$klas == k), d, k]
-          lines(statMRHLP$polynomials[, d, k], lty = "dotted", lwd = 2, col = colors[k])
-          lines(seq(1:modelMRHLP$n)[index], polynomials, lwd = 2, col = colors[k])
+          polynomials <- statMRHLP$polynomials[index, d, k]
+          lines(statMRHLP$polynomials[, d, k], col = colorsvec[k], lty = "dotted", lwd = 1)
+          lines(seq(1:modelMRHLP$n)[index], polynomials, col = colorsvec[k], lwd = 1.5)
         }
       }
 
       # Probablities of the hidden process (segmentation)
-      plot.default(statMRHLP$piik[, 1], type = "l", lwd = 2, col = colors[1], xlab = "x", ylab = expression('Probability ' ~ pi [k] (t, w)))
+      plot.default(statMRHLP$piik[, 1], type = "l", xlab = "x", ylab = expression('Probability ' ~ pi [k] (t, w)), col = colorsvec[1], lwd = 1.5)
       if (modelMRHLP$K > 1) {
         for (k in 2:modelMRHLP$K) {
-          lines(statMRHLP$piik[, k], type = "l", lwd = 2, col = colors[k])
+          lines(statMRHLP$piik[, k], col = colorsvec[k], lwd = 1.5)
         }
       }
 
       # Data, regression model, and segmentation
-      par(mfrow = c(2, 1))
-      matplot(modelMRHLP$Y, type = "l", lty = 1, col = gray.colors(modelMRHLP$m), ylab = "y", xlab = "")
+      par(mfrow = c(2, 1), mai = c(0.6, 1, 0.5, 0.5), mgp = c(2, 1, 0))
+      matplot(modelMRHLP$Y, type = "l", ylim = yaxislim, xlab = "x", ylab = "y", col = gray.colors(modelMRHLP$m), lty = 1)
       title(main = "Time series, estimated MRHLP model, and segmentation")
 
       # Transition time points
       tk = which(diff(statMRHLP$klas) != 0)
       for (i in 1:length(tk)) {
-        abline(v = tk[i], lty = "dotted", lwd = 2, col = "red")
+        abline(v = tk[i], col = "red", lty = "dotted", lwd = 1.5)
       }
-      # Probablities of the hidden process (segmentation)
-      plot.default(statMRHLP$klas, type = "l", lwd = 2, col = "red", xlab = "", ylab = "Estimated class labels")
 
-      # Model log-likelihood during EM
-      par(mfrow = c(1, 1))
-      plot.default(unlist(statMRHLP$stored_loglik)[5:35], type = "l", col = "blue", xlab = "EM iteration number", ylab = "log-lokelihodd")
+      # Probablities of the hidden process (segmentation)
+      plot.default(statMRHLP$klas, type = "l", xlab = "", ylab = "Estimated class labels", col = "red", lwd = 2)
+
+      # # Model log-likelihood during EM
+      # par(mfrow = c(1, 1))
+      # plot.default(unlist(statMRHLP$stored_loglik), type = "l", xlab = "EM iteration number", ylab = "log-lokelihodd", col = "blue")
     }
   )
 )
--- MRHLPr/R/main_MRHLP.R
+++ MRHLPr/R/main_MRHLP.R
@@ -1,49 +1,48 @@
-"
-% Segmentation of multivariate time series with a Multiple Regression
-% model with a Hidden Logistic Process (MRHLP).
-%
-%% Please cite the following papers for this code:
-%
-% @article{Chamroukhi-MRHLP-2013,
-% 	Author = {F. Chamroukhi and D. Trabelsi and S. Mohammed and L. Oukhellou and Y. Amirat},
-% 	Journal = {Neurocomputing},
-% 	Month = {November},
-% 	Pages = {633--644},
-% 	Publisher = {Elsevier},
-% 	Title = {Joint segmentation of multivariate time series with hidden process regression for human activity recognition},
-% 	Volume = {120},
-% 	Year = {2013},
-% 	note = {},
-% 	url  = {https://chamroukhi.com/papers/chamroukhi_et_al_neucomp2013b.pdf}
-% 	}
-%
-% @article{Chamroukhi-FDA-2018,
-%  	Journal = {Wiley Interdisciplinary Reviews: Data Mining and Knowledge Discovery},
-%  	Author = {Faicel Chamroukhi and Hien D. Nguyen},
-%  	Note = {DOI: 10.1002/widm.1298.},
-%  	Volume = {},
-%  	Title = {Model-Based Clustering and Classification of Functional Data},
-%  	Year = {2019},
-%  	Month = {to appear},
-%  	url =  {https://chamroukhi.com/papers/MBCC-FDA.pdf}
-%     }
-%
-% @article{chamroukhi_et_al_NN2009,
-% 	Address = {Oxford, UK, UK},
-% 	Author = {Chamroukhi, F. and Sam\'{e}, A. and Govaert, G. and Aknin, P.},
-% 	Date-Added = {2014-10-22 20:08:41 +0000},
-% 	Date-Modified = {2014-10-22 20:08:41 +0000},
-% 	Journal = {Neural Networks},
-% 	Number = {5-6},
-% 	Pages = {593--602},
-% 	Publisher = {Elsevier Science Ltd.},
-% 	Title = {Time series modeling by a regression approach based on a latent process},
-% 	Volume = {22},
-% 	Year = {2009},
-% 	url  = {https://chamroukhi.com/papers/Chamroukhi_Neural_Networks_2009.pdf}
-% 	}
-%
-"
+# Segmentation of multivariate time series with a Multiple Regression
+# model with a Hidden Logistic Process (MRHLP).
+#
+## Please cite the following papers for this code:
+#
+# @article{Chamroukhi-MRHLP-2013,
+# 	Author = {F. Chamroukhi and D. Trabelsi and S. Mohammed and L. Oukhellou and Y. Amirat},
+# 	Journal = {Neurocomputing},
+# 	Month = {November},
+# 	Pages = {633--644},
+# 	Publisher = {Elsevier},
+# 	Title = {Joint segmentation of multivariate time series with hidden process regression for human activity recognition},
+# 	Volume = {120},
+# 	Year = {2013},
+# 	note = {},
+# 	url  = {https://chamroukhi.com/papers/chamroukhi_et_al_neucomp2013b.pdf}
+# 	}
+#
+# @article{Chamroukhi-FDA-2018,
+#  	Journal = {Wiley Interdisciplinary Reviews: Data Mining and Knowledge Discovery},
+#  	Author = {Faicel Chamroukhi and Hien D. Nguyen},
+#  	Note = {DOI: 10.1002/widm.1298.},
+#  	Volume = {},
+#  	Title = {Model-Based Clustering and Classification of Functional Data},
+#  	Year = {2019},
+#  	Month = {to appear},
+#  	url =  {https://chamroukhi.com/papers/MBCC-FDA.pdf}
+#     }
+#
+# @article{chamroukhi_et_al_NN2009,
+# 	Address = {Oxford, UK, UK},
+# 	Author = {Chamroukhi, F. and Sam\'{e}, A. and Govaert, G. and Aknin, P.},
+# 	Date-Added = {2014-10-22 20:08:41 +0000},
+# 	Date-Modified = {2014-10-22 20:08:41 +0000},
+# 	Journal = {Neural Networks},
+# 	Number = {5-6},
+# 	Pages = {593--602},
+# 	Publisher = {Elsevier Science Ltd.},
+# 	Title = {Time series modeling by a regression approach based on a latent process},
+# 	Volume = {22},
+# 	Year = {2009},
+# 	url  = {https://chamroukhi.com/papers/Chamroukhi_Neural_Networks_2009.pdf}
+# 	}
+#
+
 rm(list = ls())
 source("R/FData.R")
 source("R/ModelMRHLP.R")
@@ -52,7 +51,19 @@ source("R/ModelLearner.R")
 
 
 # Building matrices for regression
+
+# Toy multivariate time series with regime changes
+# Y = cbind(c(rnorm(100, mean = 0), rnorm(120, mean = 7), rnorm(200, mean = 4), rnorm(100, mean = -1), rnorm(150, mean = 3.5)),
+#            c(rnorm(100, mean = 1), rnorm(120, mean = 5), rnorm(200, mean = 6), rnorm(100, mean = -2), rnorm(150, mean = 2)),
+#            c(rnorm(100, mean = -2), rnorm(120, mean = 10), rnorm(200, mean = 8), rnorm(100, mean = 0), rnorm(150, mean = 5)))
+# X = matrix(seq(from = 0, to = 1, length.out = nrow(Y)), nrow = 1)
+
+# Toy time series with regime changes
 load("data/simulatedTimeSeries.RData")
+
+# Some real time series with regime changes
+# load("data/realTimeSeries.RData")
+
 fData <- FData$new()
 fData$setData(X, Y)
 
@@ -82,10 +93,8 @@ solution$plot()
 
 
 
-
 ####
 # CEM Algorithm
 ####
 #solution <- CEM(modelMRHLP, n_tries, max_iter, threshold, verbose, verbose_IRLS)
 #solution$plot()
-
